2c94c3c3aad7e64bb61087c059a857a4e82fac1f
smaraaosp <smara@google.com>
2017-11-17 12:52:24 -0800

    Added LOCAL_INSTRUMENT_SOURCE_DIRS to capture code coverage.
    Bug:69471819
    
    Test: m RunCarMessengerRoboTests-jacoco
    Change-Id: I710aa38bff2f114bc69092e04b7a8eddbd8ad8c9

--- tests/robotests/Android.mk
+++ tests/robotests/Android.mk
@@ -8,7 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 # Include the testing libraries (JUnit4 + Robolectric libs).
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    truth-prebuilt
+    truth-prebuilt \
+    mockito-robolectric-prebuilt
 
 LOCAL_JAVA_LIBRARIES := \
     junit \
@@ -36,4 +37,10 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_TEST_PACKAGE := CarMessengerApp
 
-include prebuilts/misc/common/robolectric/3.1.1/run_robotests.mk
+LOCAL_ROBOTEST_FILES := $(filter-out %/BaseRobolectricTest.java,\
+    $(call find-files-in-subdirs,$(LOCAL_PATH)/src,*Test.java,.))
+
+LOCAL_INSTRUMENT_SOURCE_DIRS := $(dir $(LOCAL_PATH))../src
+
+#Require Robolectric 3.4.2 since the min-SDK target is 24
+include prebuilts/misc/common/robolectric/3.4.2/run_robotests.mk
