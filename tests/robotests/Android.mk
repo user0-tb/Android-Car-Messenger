30a63fd237cea572552b4fe01f3d70f84c58f99b
James Lemieux <jplemieux@google.com>
2017-12-18 13:37:36 -0800

    Adopt robolectric 3.5.1
    Bug: 70689021
    Test: make RunCarMessengerRoboTests
    Change-Id: I9f1c37fd260b55ce61a6b408376613e9fd818f55

--- tests/robotests/Android.mk
+++ tests/robotests/Android.mk
@@ -13,7 +13,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_JAVA_LIBRARIES := \
     junit \
-    platform-robolectric-3.1.1-prebuilt \
+    platform-robolectric-3.5.1-prebuilt \
     sdk_vcurrent
 
 LOCAL_INSTRUMENTATION_FOR := CarMessengerApp
@@ -37,10 +37,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_TEST_PACKAGE := CarMessengerApp
 
-LOCAL_ROBOTEST_FILES := $(filter-out %/BaseRobolectricTest.java,\
-    $(call find-files-in-subdirs,$(LOCAL_PATH)/src,*Test.java,.))
-
 LOCAL_INSTRUMENT_SOURCE_DIRS := $(dir $(LOCAL_PATH))../src
 
-#Require Robolectric 3.4.2 since the min-SDK target is 24
-include prebuilts/misc/common/robolectric/3.4.2/run_robotests.mk
+include prebuilts/misc/common/robolectric/3.5.1/run_robotests.mk
