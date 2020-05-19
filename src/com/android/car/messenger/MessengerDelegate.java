be2972b7f8f5b153675c7ef6362841b015e4c1dc
Ritwika Mitra <ritwikam@google.com>
2020-05-19 15:35:25 -0700

    DO NOT MERGE Refactor to use new ProjectionStateListener apis
    This is a cherrypick backport of ag/10815186
    
    Bug: 155220140
    Test: built app successfully, verified notifications suppressed correctly,
    verified no service connection leaks when user switched.
    
    Change-Id: I349e1ed107cef5cc26919c07a0cb601e5884e85d

--- src/com/android/car/messenger/MessengerDelegate.java
+++ src/com/android/car/messenger/MessengerDelegate.java
@@ -75,7 +75,6 @@ public class MessengerDelegate implements BluetoothMonitor.OnBluetoothEventListe
         mContext = context;
 
         mProjectionStateListener = new ProjectionStateListener(context);
-        mProjectionStateListener.start();
 
         mNotificationManager =
                 (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -239,7 +238,6 @@ public class MessengerDelegate implements BluetoothMonitor.OnBluetoothEventListe
         if (mPhoneNumberInfoFuture != null) {
             mPhoneNumberInfoFuture.cancel(true);
         }
-        mProjectionStateListener.stop();
     }
 
     /**
